21407efc7d5fabf27b6ada5a7c67f919108032c8
acheh <andrii.chehrynets@gmail.com>
2023-04-08 16:01:38 +0200

    changed project structure and "rebranding"

--- recipe-manager-api/src/main/java/org/acheh/demo/recipe/manager/api/repository/RecipeRepository.java
+++
@@ -1,9 +0,0 @@
-package org.acheh.demo.recipe.manager.api.repository;
-
-import org.acheh.demo.recipe.manager.api.model.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-}
--- recipe-manager-api/src/main/java/org/acheh/demo/recipe/manager/api/service/RecipeService.java
+++
@@ -1,7 +0,0 @@
-package org.acheh.demo.recipe.manager.api.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class RecipeService {
-}
