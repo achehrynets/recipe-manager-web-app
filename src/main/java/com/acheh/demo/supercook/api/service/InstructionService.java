b819f47995f4f522e2b8790e8029b683e607aed1
acheh <andrii.chehrynets@gmail.com>
2023-04-11 22:54:09 +0200

    added service interfaces

--- src/main/java/com/acheh/demo/supercook/api/service/InstructionService.java
+++ src/main/java/com/acheh/demo/supercook/api/service/InstructionService.java
@@ -1,11 +1,23 @@
 package com.acheh.demo.supercook.api.service;
 
 import com.acheh.demo.supercook.api.repository.model.Instruction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-public interface InstructionService extends BaseService<Instruction, Integer> {
+public interface InstructionService {
 
-    List<Instruction> saveAll(List<Instruction> instructions);
+    List<Instruction> update(Integer recipeId, List<Instruction> instructions);
+    List<Instruction> save(Integer recipeId, List<Instruction> instructions);
+
+    void delete(Integer recipeId, Integer instructionId);
+
+    /**
+     * Find all instructions based on search criteria
+     * @param pageable pagination information
+     * @return page of entities
+     */
+    Page<Instruction> find(Integer recipeId, String search, Pageable pageable);
 
 }
--- src/main/java/com/acheh/demo/supercook/api/service/RecipeService.java
+++ src/main/java/com/acheh/demo/supercook/api/service/RecipeService.java
@@ -1,6 +1,8 @@
 package com.acheh.demo.supercook.api.service;
 
+import com.acheh.demo.supercook.api.repository.model.Category;
 import com.acheh.demo.supercook.api.repository.model.Recipe;
+import com.acheh.demo.supercook.api.repository.model.RecipeIngredient;
 import com.acheh.demo.supercook.api.rest.v1.dto.RecipeFilterDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,4 +10,15 @@ import org.springframework.data.domain.Pageable;
 public interface RecipeService extends BaseService<Recipe, Integer> {
 
     Page<Recipe> find(RecipeFilterDto search, Pageable pageable);
+
+    void removeRecipeFromCategory(Integer id, Integer categoryId);
+
+    Category addRecipeToCategory(Integer id, Integer categoryId);
+
+    RecipeIngredient addIngredientToRecipe(Integer recipeId, Integer ingredientId, RecipeIngredient recipeIngredient);
+
+    void removeIngredientFromRecipe(Integer recipeId, Integer ingredientId);
+
+    RecipeIngredient updateIngredientInRecipe(Integer recipeId, Integer ingredientId, RecipeIngredient recipeIngredient);
+
 }
