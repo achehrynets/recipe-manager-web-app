86fffaf441dac0dbd55a606b6167b76f8dd4134b
acheh <andrii.chehrynets@gmail.com>
2023-04-11 22:55:54 +0200

    added EntityNotFoundException custom exception

--- src/main/java/com/acheh/demo/supercook/api/exception/EntityNotFoundException.java
+++ src/main/java/com/acheh/demo/supercook/api/exception/EntityNotFoundException.java
@@ -3,10 +3,10 @@ package com.acheh.demo.supercook.api.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class BadRequestException extends RuntimeException {
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EntityNotFoundException extends RuntimeException {
 
-    public BadRequestException(String message) {
+    public EntityNotFoundException(String message) {
         super(message);
     }
 
