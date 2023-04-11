47744b5f097f366ea1d7d72b92d5210854bb19dd
acheh <andrii.chehrynets@gmail.com>
2023-04-11 18:31:45 +0200

    added java doc to BaseService interface

--- src/main/java/com/acheh/demo/supercook/api/service/BaseService.java
+++ src/main/java/com/acheh/demo/supercook/api/service/BaseService.java
@@ -3,6 +3,11 @@ package com.acheh.demo.supercook.api.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+/**
+ * Base service interface
+ * @param <E> entity generic type
+ * @param <ID> entity id generic type
+ */
 public interface BaseService<E, ID> {
 
     E getById(ID id);
