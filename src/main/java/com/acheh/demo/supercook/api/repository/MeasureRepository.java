b1ca2780f4139bbe6d155cf363d8bd9525f648f5
acheh <andrii.chehrynets@gmail.com>
2023-04-11 18:28:45 +0200

    removed obsolete files

--- src/main/java/com/acheh/demo/supercook/api/repository/MeasureRepository.java
+++
@@ -1,9 +0,0 @@
-package com.acheh.demo.supercook.api.repository;
-
-import com.acheh.demo.supercook.api.model.Measure;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MeasureRepository extends JpaRepository<Measure, Integer> {
-}
