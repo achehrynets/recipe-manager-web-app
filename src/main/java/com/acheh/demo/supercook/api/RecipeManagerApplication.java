86c37ffda335a462ee2fd04d845f191f79cdc432
acheh <andrii.chehrynets@gmail.com>
2023-04-11 18:33:27 +0200

    enabled jpa repository and added support of entity graph

--- src/main/java/com/acheh/demo/supercook/api/RecipeManagerApplication.java
+++ src/main/java/com/acheh/demo/supercook/api/RecipeManagerApplication.java
@@ -1,9 +1,15 @@
 package com.acheh.demo.supercook.api;
 
+import com.cosium.spring.data.jpa.entity.graph.repository.support.EntityGraphJpaRepositoryFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/**
+ * Main application class.
+ */
 @SpringBootApplication
+@EnableJpaRepositories(repositoryFactoryBeanClass = EntityGraphJpaRepositoryFactoryBean.class)
 public class RecipeManagerApplication {
 
     public static void main(String[] args) {
